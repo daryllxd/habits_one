c4928b2d1638885e7e915948a343a7954688b1b6
Daryll Santos <daryll.santos@gmail.com>
2018-02-25 17:43:51 +0800

    Added registration_changeset for Users.

--- web/models/user.ex
+++ web/models/user.ex
@@ -16,4 +16,23 @@ defmodule HabitsOne.User do
     |> validate_length(:name, min: 1, max: 20)
     |> validate_length(:email, min: 1, max: 20)
   end
+
+  # Different context if when signing up
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_required(:name)
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
 end
